d926bba774f1dee161d1ad67d36bad3533276621
maxim <maximum454@gmail.com>
2019-02-26 15:55:50 +0600

    Проект

---
+++ eks-electrik/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'bootstrap';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/dropdown';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
